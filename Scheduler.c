831895711b0032b0be682ee12fc3bbd83d2d73a3
Craig Comberbach <craigc@price-hvac.com>
2016-05-26 22:07:45 -0500

    Most functionality implemented
    Does not implement limited recurrence tasks, only permanent ones
    Still missing timing statistics (individual & global) entirely

--- Scheduler.c
+++ Scheduler.c
@@ -1,3 +1,7 @@
+//Add individual task timing statistics
+//Add global timing statistics
+//Doesn't fully support a limited number of occurances
+
 /**************************************************************************************************
 Authours:				Craig Comberbach
 Target Hardware:		PIC24F
@@ -13,7 +17,7 @@ Version History:
 #include "Scheduler.h"
 
 /************* Semantic Versioning***************/
-#if SCHEDULER_MAJOR != 1
+#if SCHEDULER_MAJOR != 0
 	#error "Scheduler library has had a change that loses some previously supported functionality"
 #elif SCHEDULER_MINOR != 0
 	#error "Scheduler library has new features that this code may benefit from"
@@ -26,7 +30,143 @@ Version History:
 /*************    Enumeration     ***************/
 /***********State Machine Definitions************/
 /*************  Global Variables  ***************/
+#ifdef CONSERVE_MEMORY
+	uint16_t schedulerPeriod_mS;
+#else
+	uint32_t schedulerPeriod_mS;
+#endif
+
+struct SCHEDULED_TASKS
+{
+	#ifdef CONSERVE_MEMORY
+		void (*task)(uint16_t);
+		uint16_t initialDelay_mS;
+		uint16_t period_mS;
+		uint16_t elapsedTime_mS;
+		uint16_t recurrence;
+		uint16_t recurrenceCount;
+		uint16_t minExecutionTime_uS;
+		uint16_t avgExecutionTime_uS;
+		uint16_t maxExecutionTime_uS;
+		uint16_t currentExecutionTime_uS;
+	#else
+		void (*task)(uint32_t);
+		uint32_t initialDelay_mS;
+		uint32_t period_mS;
+		uint32_t elapsedTime_mS;
+		uint32_t recurrence;
+		uint32_t recurrenceCount;
+		uint32_t minExecutionTime_uS;
+		uint32_t avgExecutionTime_uS;
+		uint32_t maxExecutionTime_uS;
+		uint32_t currentExecutionTime_uS;
+	#endif
+} scheduledTasks[NUMBER_OF_SCHEDULED_TASKS];
+uint32_t minGlobalExecutionTime_uS;
+uint32_t avgGlobalExecutionTime_uS;
+uint32_t maxGlobalExecutionTime_uS;
+int16_t delayFlag = 0;
+
 /*************Function  Prototypes***************/
+void __attribute__((interrupt, auto_psv)) _T1Interrupt(void);
+
 /************* Device Definitions ***************/
 /************* Module Definitions ***************/
 /************* Other  Definitions ***************/
+
+void Task_Master_2000(void)
+{
+	int16_t loop;
+	
+	for(loop = 0; loop < NUMBER_OF_SCHEDULED_TASKS; ++loop)
+	{
+		ClrWdt();
+		if(scheduledTasks[loop].initialDelay_mS <= schedulerPeriod_mS)
+		{
+			if(scheduledTasks[loop].recurrenceCount > 0)
+			{
+				if(scheduledTasks[loop].elapsedTime_mS >= scheduledTasks[loop].period_mS)
+				{
+//Record time for future calculation
+					scheduledTasks[loop].task(scheduledTasks[loop].elapsedTime_mS);			//Run the current task, send the time since last execution
+//Snap current time for min/avg/max time evaluation
+					if(++scheduledTasks[loop].recurrenceCount == 0)
+						scheduledTasks[loop].recurrenceCount = 1;
+				}
+				else
+				{
+					scheduledTasks[loop].elapsedTime_mS += schedulerPeriod_mS;				//Continue towards NEXT execution
+				}
+			}
+			else
+			{
+//Record time for future calculation
+				scheduledTasks[loop].task(0);												//Run the current task, indicate this is the first time running (0mS)
+//Snap current time for min/avg/max time evaluation
+				scheduledTasks[loop].recurrenceCount = 1;
+				
+			}
+		}
+		else
+		{
+			scheduledTasks[loop].initialDelay_mS -= schedulerPeriod_mS;						//Continue towards FIRST execution
+		}
+			
+	}
+
+	delayFlag = 0;
+
+	return;
+}
+
+void Initialize_Scheduler(uint32_t newPeriod_mS)
+{
+//	I need Instruction clock (FCY Hz)
+//	I need period (mS))
+//	I need Number of schedulable tasks
+	if(newPeriod_mS != 0)
+		schedulerPeriod_mS = newPeriod_mS;
+	else
+		while(1);//TODO - DEBUG ME! I should never execute
+
+	return;
+}
+
+void Schedule_Task(enum SCHEDULER_DEFINITIONS taskName, void (*newTask)(uint32_t), uint16_t newInitialDelay_mS, uint16_t newPeriod_mS, uint16_t newRepetitions)
+{
+	//Task Information
+	if(*newTask  != (void*)0)
+		scheduledTasks[taskName].task = newTask;
+	else
+		while(1);//TODO - DEBUG ME! I should never execute
+
+	//Timing Information
+	scheduledTasks[taskName].initialDelay_mS = newInitialDelay_mS;
+	scheduledTasks[taskName].period_mS = newPeriod_mS;
+	scheduledTasks[taskName].elapsedTime_mS = 0;
+
+	//Recurrence Information
+	scheduledTasks[taskName].recurrence = newRepetitions;
+	scheduledTasks[taskName].recurrenceCount = 0;
+
+	//Runtime Statistics Information
+	scheduledTasks[taskName].currentExecutionTime_uS = 0;
+	scheduledTasks[taskName].minExecutionTime_uS = ~0;
+	scheduledTasks[taskName].avgExecutionTime_uS = 0;
+	scheduledTasks[taskName].maxExecutionTime_uS = 0;
+	
+	return;
+}
+
+int8_t Waiting_To_Run_Tasks(void)
+{
+	return delayFlag;
+}
+
+void __attribute__((interrupt, auto_psv)) _T1Interrupt(void)
+{
+	delayFlag = 1;
+	TMR1 = 0;
+	IFS0bits.T1IF = 0;
+	return;
+}
--- Scheduler.h
+++ Scheduler.h
@@ -1,14 +1,38 @@
 #ifndef SCHEDULER_H
 #define	SCHEDULER_H
 
+/*
+Instructions for adding to a new project:
+*/
+
+/***********Add to config file header************/
+/*
+//A2D Library
+#define SCHEDULER_MAJOR	0
+#define SCHEDULER_MINOR	0
+#define SCHEDULER_PATCH	0
+*/
+
+/***************Add to config file***************/
+/*
+#ifndef SCHEDULER_LIBRARY
+	#error "You need to include the Scheduler library for this code to compile"
+#endif
+ */
+
 /************* Semantic Versioning***************/
 #define SCHEDULER_LIBRARY
 
 /*************   Magic  Numbers   ***************/
+#define PERMANENT_TASK	0
+
 /*************    Enumeration     ***************/
-/*************Structure Definitions**************/
 /***********State Machine Definitions************/
 /*************Function  Prototypes***************/
+void Task_Master_2000(void);
+void Initialize_Scheduler(uint32_t newPeriod_mS);
+void Schedule_Task(enum SCHEDULER_DEFINITIONS taskDuJour, void (*newTask)(uint32_t), uint16_t newInitialDelay_mS, uint16_t newPeriod_mS, uint16_t newRepetitions);
+int8_t Waiting_To_Run_Tasks(void);
 
 #endif	/* SCHEDULER_H */
 
